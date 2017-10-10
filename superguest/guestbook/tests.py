cd3ae22bb9f9e8cee24e1d24a363d81666426366
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-10 16:27:12 +0100

    ut added

--- superguest/guestbook/tests.py
+++ superguest/guestbook/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest (TestCase) :
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
