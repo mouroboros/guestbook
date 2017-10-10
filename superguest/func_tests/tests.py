2cd7f91a52967e982a3612130ab42981d24d7946
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-10 16:03:51 +0100

    ft skeleton

--- superguest/func_tests/tests.py
+++ superguest/func_tests/tests.py
@@ -1,4 +1,15 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
-browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+import time
+import os, unittest
+
+class BlogTests (LiveServerTestCase):
+       def setUp(self):
+           self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+
+       def tearDown (self):
+            self.browser.quit()
+
+       # Insert our user story here
--- test.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
