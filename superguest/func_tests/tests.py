e5dc03fbaae2b97abcc28078528f94419bd26c08
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-10 17:56:07 +0100

    extended FT - fails

--- superguest/func_tests/tests.py
+++ superguest/func_tests/tests.py
@@ -8,7 +8,8 @@ import os, unittest
 class GuestTests (LiveServerTestCase):
        def setUp(self):
            self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-
+           self.browser.implicitly_wait(3)
+           
        def tearDown (self):
             self.browser.quit()
 
@@ -22,6 +23,18 @@ class GuestTests (LiveServerTestCase):
               # John notices the guesthouse title
               self.assertIn("Guesthouse",self.browser.title)
               # John is invited to leave a comment
+              inputbox = self.browser.find_element_by_id('id_new_comment')
+              self.assertEqual(
+                     inputbox.get_attribute('placeholder'),
+                     'Please enter a comment')
               # John types "A very nice stay"
+              inputbox.send_keys('A very nice stay')
               # John hits enter, the page updates
+              inputbox.send_keys(Keys.ENTER)
+              time.sleep(5)
               # and the new page shows John's comment on the webpage
+              table = self.browser.find_element_by_id('id_comment_table')
+              rows = table.find_elements_by_tag_name('tr')
+              self.assertTrue(
+                     any(row.text == 'A new nice stay')
+                     )
