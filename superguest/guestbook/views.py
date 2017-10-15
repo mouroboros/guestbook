1be6a2da3544d33486ddbfecbdb09fd64214c4a2
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-15 17:15:54 +0100

    new views

--- superguest/guestbook/views.py
+++ superguest/guestbook/views.py
@@ -3,11 +3,13 @@ from guestbook.models import Comment
 
 
 def home_page(request):
-    comment = Comment()
-    comment.text = request.POST.get('comment_text', '')
-    comment.save()
-    
+    if request.method == 'POST' :
+        new_comment_text = request.POST['comment_text']
+        Comment.objects.create(text=new_comment_text)
+    else:
+        new_comment_text = ''
+        
     return render(request, 'home.html', {
-        'new_comment_text' : comment.text,
+        'new_comment_text' : new_comment_text,
         })
         
