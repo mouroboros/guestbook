9436298201044cf4dd629d852a6748d3eb5d9fa6
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-15 16:43:37 +0100

    database ut

--- superguest/guestbook/tests.py
+++ superguest/guestbook/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from guestbook.models import Comment
 
 
 class HomePageTest (TestCase) :
@@ -20,3 +21,24 @@ class HomePageTest (TestCase) :
         response = self.client.post('/', data={'comment_text': 'Very peaceful'})
         self.assertIn('Very peaceful', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class CommentModelTest(TestCase) :
+
+    def test_saving_and_retriving_comments (self) :
+
+        first_comment = Comment()
+        first_comment.text = "the first comment"
+        first_comment.save()
+
+        second_comment = Comment()
+        second_comment.text = "the second comment"
+        second_comment.save()
+
+        saved_comments = Comment.objects.all()
+        self.assertEqual(saved_comments.count(),2)
+
+        first_saved_comment = saved_comments[0]
+        second_saved_comment = saved_comments[1]
+        self.assertEqual(first_saved_comment.text, 'the first comment')
+        self.assertEqual(second_saved_comment.text, 'the second comment')
+    
