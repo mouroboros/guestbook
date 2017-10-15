414c196eb4538d9764b28239a8d49996e56341b6
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-15 17:24:34 +0100

    views changed - ut passing

--- superguest/guestbook/views.py
+++ superguest/guestbook/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from guestbook.models import Comment
 
 
@@ -6,10 +6,7 @@ def home_page(request):
     if request.method == 'POST' :
         new_comment_text = request.POST['comment_text']
         Comment.objects.create(text=new_comment_text)
-    else:
-        new_comment_text = ''
+        return redirect('/')
         
-    return render(request, 'home.html', {
-        'new_comment_text' : new_comment_text,
-        })
+    return render(request, 'home.html')
         
