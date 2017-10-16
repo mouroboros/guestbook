594325ae39dc928f838004996307a48f5a55ae7b
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-16 17:39:37 +0100

    bootstrap working, fixed ut

--- superguest/guestbook/tests.py
+++ superguest/guestbook/tests.py
@@ -11,7 +11,7 @@ class HomePageTest (TestCase) :
         response = self.client.get('/')  
 
         html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
         self.assertIn('<title>Le Corbusier Guesthouse </title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
